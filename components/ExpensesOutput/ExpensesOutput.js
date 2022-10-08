1a3c737e7612fd135d903e4afb1071b7af25a91e
Callum :D <111291022+CallumAckery@users.noreply.github.com>
2022-10-08 23:43:16 +0100

    ss

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,60 +4,7 @@ import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
 
-const DUMMY_EXP = [
-    {
-        id: 'e1',
-        description: 'A piar of boots',
-        amount: 59.99,
-        date: new Date('2022-09-25'),
-    },
-    {
-        id: 'e2',
-        description: 'Trousers',
-        amount: 89.24,
-        date: new Date('2022-09-23'),
-    },{
-        id: 'e3',
-        description: 'Some Apples',
-        amount: 5.99,
-        date: new Date('2022-09-24'),
-    },
-    {
-        id: 'e4',
-        description: 'A piar of boots',
-        amount: 59.99,
-        date: new Date('2022-09-25'),
-    },
-    {
-        id: 'e5',
-        description: 'Trousers',
-        amount: 89.24,
-        date: new Date('2022-09-23'),
-    },{
-        id: 'e6',
-        description: 'Some Apples',
-        amount: 5.99,
-        date: new Date('2022-09-24'),
-    },
-    {
-        id: 'e7',
-        description: 'A piar of boots',
-        amount: 59.99,
-        date: new Date('2022-09-25'),
-    },
-    {
-        id: 'e8',
-        description: 'Trousers',
-        amount: 89.24,
-        date: new Date('2022-09-23'),
-    },
-    {
-        id: 'e9',
-        description: 'Some Apples',
-        amount: 5.99,
-        date: new Date('2022-09-24'),
-    },
-]
+
 
 
 
